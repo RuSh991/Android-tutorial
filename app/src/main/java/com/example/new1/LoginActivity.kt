18dc3998fbd7b360c7360d381a86aba2f229f382
Ruchita <sharmaruchita066@gmail.com>
2021-05-29 02:44:18 +0530

    Bottom View

--- app/src/main/java/com/example/new1/LoginActivity.kt
+++ app/src/main/java/com/example/new1/LoginActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.new1.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +17,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var x=findNavController(R.id.f1)
+
+        val navController=findNavController(R.id.fragment)
+        val appBarConfiguration= AppBarConfiguration(setOf(R.id.fragmentOne,R.id.fragmentTwo,R.id.fragmentThree) )
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }

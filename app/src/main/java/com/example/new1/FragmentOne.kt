cb9eec322f3c107a25326aab5c12a41a6a69f574
Ruchita <sharmaruchita066@gmail.com>
2021-06-01 22:45:40 +0530

    Bottom Navigation

--- app/src/main/java/com/example/new1/FragmentOne.kt
+++ app/src/main/java/com/example/new1/FragmentOne.kt
@@ -11,23 +11,28 @@ import com.example.new1.databinding.FragmentOneBinding
 
 class FragmentOne : Fragment() {
     private var _binding: FragmentOneBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding= FragmentOneBinding.inflate(inflater,container,false)
+
+        _binding = FragmentOneBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textView.setOnClickListener {
-            findNavController().navigate(R.id.action_fragmentOne_to_fragmentTwo)
+        binding.button.setOnClickListener {
+            if (binding.editTextTextEmailAddress.text.toString()=="xxx@gmail.com"){
+                findNavController().navigate(R.id.action_fragmentOne_to_fragmentTwo)
+            }
         }
     }
 }

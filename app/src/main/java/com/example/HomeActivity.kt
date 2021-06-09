aa8c745804be47766659c728f5ac4fb9a7a4f035
Ruchita <sharmaruchita066@gmail.com>
2021-06-09 21:26:10 +0530

    Spinner

--- app/src/main/java/com/example/HomeActivity.kt
+++ app/src/main/java/com/example/HomeActivity.kt
@@ -1,19 +1,32 @@
 package com.example
 
+import android.graphics.ColorSpace
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.new1.databinding.ActivityHomeBinding
 import java.lang.NullPointerException
 import android.view.*
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.Toast
 import com.example.new1.R
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding:ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.cardy.visibility=View.VISIBLE
+        setContentView(R.layout.activity_home)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        binding.spMonths.onItemSelectedListener=object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                Toast.makeText(this@HomeActivity,"You have selected ${adapterView?.getItemAtPosition(position).toString()}", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 }

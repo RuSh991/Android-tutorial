ac1a04e7ee9e942035e008b59dda750269ba53bf
Ruchita <sharmaruchita066@gmail.com>
2021-06-14 21:41:29 +0530

    Run Blocking

--- app/src/main/java/com/example/FirstFragment.kt
+++ app/src/main/java/com/example/FirstFragment.kt
@@ -18,62 +18,35 @@ import okhttp3.Dispatcher
 
 class FirstFragment : Fragment() {
 
-    private val scope= CoroutineScope(CoroutineName("Myscope"))
-    private var _binding: FragmentFirstBinding?=null
+    private val scope = CoroutineScope(CoroutineName("Myscope"))
+    private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ):View? {
+    ): View? {
         // Inflate the layout for this fragment
-        _binding= FragmentFirstBinding.inflate(inflater,container, false)
-        val mainJob=scope.launch {
-            val job1=launch{
-                while(true){
-                    yield()
-                    Log.d("Coroutine","Job 1 Running...")
-                }
-            }
-            val job2=launch {
-                Log.d("Coroutine","Job 2 Running...")
-            }
-            delay(1000L)
-            Log.d("Coroutine","Job 1 Cancelling...")
-            job2.cancelAndJoin()
-            Log.d("Coroutine","Job 1 Cancelled")
-        }
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
         runBlocking {
-            delay(2000L)
-            Log.d("Coroutine","Main job Cancelling...")
-            mainJob.cancelAndJoin()
-            Log.d("Coroutine","Main job Cancelled")
+            Log.d("Coroutine", this.coroutineContext.toString())
+            delay(5000L)
+            GlobalScope.launch {
+                Log.d("Coroutine", "Coroutine #1")
+                delay(5000L)
+            }
+            GlobalScope.launch {
+                Log.d("Coroutine", "Coroutine #2")
+                delay(5000L)
+            }
         }
+        Log.d("Coroutine", "RunBlocking completed")
+
         binding.clickButton.setOnClickListener {
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
         return binding.root
     }
-
-    override fun onPause() {
-        Log.d("Coroutine","Running...")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d("Coroutine","Running...")
-        super.onStop()
-    }
-
-    override fun onResume() {
-        Log.d("Coroutine","Running...")
-        super.onResume()
-    }
-
-    override fun onDestroy() {
-        Log.d("Coroutine","Running...")
-        super.onDestroy()
-    }
-
 }

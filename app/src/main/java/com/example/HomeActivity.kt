38261631af3cf87023dd9e209d929dde976fde21
Ruchita <sharmaruchita066@gmail.com>
2021-07-12 23:17:52 +0530

    Live Data1

--- app/src/main/java/com/example/HomeActivity.kt
+++ app/src/main/java/com/example/HomeActivity.kt
@@ -13,6 +13,7 @@ import android.widget.AdapterView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -27,35 +28,65 @@ import com.example.repository.Repository
 import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Response
 import timber.log.Timber
+
 @AndroidEntryPoint
 
 class HomeActivity : AppCompatActivity() {
-    private val viewModel by viewModels<MainViewModel>()
-    private lateinit var binding: ActivityHomeBinding
-    private lateinit var adapter: MyAdapter
 
+    private val model: NameViewModel by viewModels()
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Other code to setup the activity...
 
-        adapter = MyAdapter()
-        binding.recyclerView.adapter=adapter
+        // Create the observer which updates the UI.
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        model.currentName.observe(this){
+            binding.name.text=it.toString()
+        }
 
-        viewModel.getPost3(2)
-        viewModel.myResponse3.observe(this, Observer { response ->
-            Timber.i(response.toString())
-            if (response.isSuccessful) {
-                this.adapter.submitList(response.body()!!)
-            }
-        })
-    }
-    override fun onStart(){
-        super.onStart()
-        Timber.i("onStart")
-    }
-    override fun onDestroy(){
-        super.onDestroy()
-        Timber.i("onDestroy")
+        binding.clickButton.setOnClickListener {
+            model.startTimeCounter()
+        }
     }
 }
+//    private val viewModel by viewModels<MainViewModel>()
+//    private lateinit var binding: ActivityHomeBinding
+//    private lateinit var adapter: MyAdapter
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityHomeBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        adapter = MyAdapter()
+////        binding.recyclerView.adapter=adapter
+//        binding.password.doAfterTextChanged {
+//            var input=it.toString()
+//            if(input.matches("^(?=.*[0-9])(?=.*[a-z])([a-z0-9_-]+)\$".toRegex())){
+//                Timber.i("Matched")
+//            }
+//            else{
+//                Timber.i("Non-Matched")
+//            }
+//        }
+//        viewModel.getPost3(2)
+//        viewModel.myResponse3.observe(this, Observer { response ->
+//            Timber.i(response.toString())
+//            if (response.isSuccessful) {
+//                this.adapter.submitList(response.body()!!)
+//            }
+//        })
+//    }
+//    override fun onStart(){
+//        super.onStart()
+//        Timber.i("onStart")
+//    }
+//    override fun onDestroy(){
+//        super.onDestroy()
+//        Timber.i("onDestroy")
+//    }
+
+
--- app/src/main/java/com/example/MainViewModel.kt
+++ app/src/main/java/com/example/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,34 +12,82 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import javax.inject.Inject
+//
+//@HiltViewModel
+//
+//class MainViewModel @Inject constructor(private val repository: Repository):ViewModel() {
+//    val myResponse: MutableLiveData<Response<Post>> = MutableLiveData()
+//    val myResponse2: MutableLiveData<Response<Post>> = MutableLiveData()
+//    val myResponse3: MutableLiveData<Response<List<Post>>> = MutableLiveData()
+//
+//    fun getPost(){
+//        viewModelScope.launch {
+//            val response=repository.getPost()
+//            myResponse.value=response
+//        }
+//    }
+//
+//    fun getPost2(number:Int){
+//        viewModelScope.launch{
+//            val response=repository.getPost2(number)
+//            Log.d("Response", response.toString())
+//            myResponse2.value=response
+//        }
+//    }
+//
+//    fun getPost3(number: Int){
+//        viewModelScope.launch {
+//            val response=repository.getPost3(number)
+//            Log.d("Response", response.toString())
+//            myResponse3.value=response
+//        }
+//    }
+//}
+//@HiltViewModel
+//
+//class MainViewModel @Inject constructor(private val repository: Repository):ViewModel() {
+//    val myResponse: MutableLiveData<Response<Post>> = MutableLiveData()
+//    val myResponse2: MutableLiveData<Response<Post>> = MutableLiveData()
+//    val myResponse3: MutableLiveData<Response<List<Post>>> = MutableLiveData()
+//
+//    fun getPost(){
+//        viewModelScope.launch {
+//            val response=repository.getPost()
+//            myResponse.value=response
+//        }
+//    }
+//
+//    fun getPost2(number:Int){
+//        viewModelScope.launch{
+//            val response=repository.getPost2(number)
+//            Log.d("Response", response.toString())
+//            myResponse2.value=response
+//        }
+//    }
+//
+//    fun getPost3(number: Int){
+//        viewModelScope.launch {
+//            val response=repository.getPost3(number)
+//            Log.d("Response", response.toString())
+//            myResponse3.value=response
+//        }
+//    }
+//}
 
-@HiltViewModel
+class NameViewModel : ViewModel() {
 
-class MainViewModel @Inject constructor(private val repository: Repository):ViewModel() {
-    val myResponse: MutableLiveData<Response<Post>> = MutableLiveData()
-    val myResponse2: MutableLiveData<Response<Post>> = MutableLiveData()
-    val myResponse3: MutableLiveData<Response<List<Post>>> = MutableLiveData()
-
-    fun getPost(){
-        viewModelScope.launch {
-            val response=repository.getPost()
-            myResponse.value=response
-        }
+    val currentName: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
     }
 
-    fun getPost2(number:Int){
-        viewModelScope.launch{
-            val response=repository.getPost2(number)
-            Log.d("Response", response.toString())
-            myResponse2.value=response
-        }
-    }
+    fun startTimeCounter() {
+        object : CountDownTimer(50000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                currentName.value = (millisUntilFinished/1000).toInt()
+            }
 
-    fun getPost3(number: Int){
-        viewModelScope.launch {
-            val response=repository.getPost3(number)
-            Log.d("Response", response.toString())
-            myResponse3.value=response
-        }
+            override fun onFinish() {
+            }
+        }.start()
     }
 }
